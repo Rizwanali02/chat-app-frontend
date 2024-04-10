ff54e8ec330800f193ac3874a68d7a4f52eca0da
rizwanali02 <rizwanali02hindoli@gmail.com>
2024-04-10 13:54:57 +0530

    getconvo add id

--- frontend/src/hooks/useGetConversation.js
+++ frontend/src/hooks/useGetConversation.js
@@ -6,12 +6,13 @@ const useGetConversations = () => {
 
     const [loading, setLoading] = useState(false);
     const [conversations, setConversations] = useState([]);
+    const user = localStorage.getItem("chat-user");
 
     useEffect(() => {
         const getConversations = async () => {
             setLoading(true)
             try {
-                const res = await fetch(`${BaseUrl}/api/users`)
+                const res = await fetch(`${BaseUrl}/api/users/${user?._id}`)
                 const data = await res.json()
                 if (data.error) throw new Error(error.message)
                 setConversations(data);
