d2ef00c0ca9e5fe07ca605a541794914d55f14ac
rizwanali02 <rizwanali02hindoli@gmail.com>
2024-04-10 15:46:14 +0530

    some changes in useListenMessages

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,21 +1,26 @@
 import { useEffect } from "react";
-import { useSocketContext } from "../context/SocketContext"
+import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
-import notification from "../assets/sounds/notification.mp3"
-const useListenMessages = () => {
+import notification from "../assets/sounds/notification.mp3";
 
-    const { socket } = useSocketContext()
-    const { messages, setMessages } = useConversation()
+const useListenMessages = () => {
+    const { socket } = useSocketContext();
+    const { messages, setMessages } = useConversation();
 
     useEffect(() => {
-        socket?.on("newMessage", (newMessage) => {
-            newMessage.shouldShake = true;
-            const sound = new Audio(notification)
-            sound.play()
-            setMessages([...messages, newMessage])
-        });
-        return () => socket?.off("newMessage")
-    }, [socket, setMessages, messages])
-}
+        if (socket) {
+            socket.on("newMessage", (newMessage) => {
+                newMessage.shouldShake = true;
+                const sound = new Audio(notification);
+                sound.play();
+                setMessages((prevMessages) => [...prevMessages, newMessage]);
+            });
 
-export default useListenMessages
+            return () => {
+                socket.off("newMessage");
+            };
+        }
+    }, [socket, setMessages]);
+};
+
+export default useListenMessages;
